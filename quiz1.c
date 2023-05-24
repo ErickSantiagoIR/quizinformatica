b81bf08fc15196bc8c86efd3c08e35db4e830943
ErickSantiagoIR <132624203+ErickSantiagoIR@users.noreply.github.com>
2023-05-23 20:15:51 -0500

    P2 - getRevertTex
    Punto 2 Quiz

--- quiz1.c
+++ quiz1.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
-#include<stdlib.h>
-
-int getInvertNumber(int numero);
-
-int main() {
-    int numero;
-    printf("Ingrese un numero: ");
-    scanf("%d", &numero);
-    
-    int invertedNumber = getInvertNumber(numero);
-
-    printf("Numero invertido: %d\n", invertedNumber);
-
-    return 0;
-}
-
-int getInvertNumber(int numero) {
-    int invertedNumber = 0;
-
-    while (numero != 0) {
-        invertedNumber = invertedNumber * 10 + numero % 10;
-        numero /= 10;
+#include <stdlib.h>
+#include <string.h>
+int main(){
+    int p_i, p_f, contador;
+    char getRevertText[20], palabra, aux;
+    int i;
+    printf("Ingrese un palabra: ");
+    scanf("%c",palabra);
+    i=0;
+    while(i<20 && palabra!='.');
+    {
+    getRevertText[i]= palabra;
+    i++;
+    scanf("%c",palabra);
+    }
+    p_i=0; p_f=i-1;
+    while(p_i<p_f)
+    {
+        aux=getRevertText[p_i];
+        getRevertText[p_i]=getRevertText[p_f];
+        getRevertText[p_f]=aux;
+        p_i++; p_f--;
     }
 
-    return invertedNumber;
+    printf("\n");
+    for(contador=0; contador<i; contador ++)
+    {
+        //printf("%c", getRevertText[contador]);
+    }
+    system("pause");
+    printf("\n\n");
+    return printf("%c", getRevertText[contador]);;
 }
+    
