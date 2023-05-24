4cd1830358d9dfcee92b5d3dbd7070e3d6faf110
ErickSantiagoIR <132624203+ErickSantiagoIR@users.noreply.github.com>
2023-05-23 20:17:22 -0500

    P3 - getInvertNumbeR
    Parte 3 Quiz

---
+++ quiz1.1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int getInvertNumber(int numero);
+
+int main() {
+    int numero;
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    
+    int invertedNumber = getInvertNumber(numero);
+
+    printf("Numero invertido: %d\n", invertedNumber);
+
+    return 0;
+}
+
+int getInvertNumber(int numero) {
+    int invertedNumber = 0;
+
+    while (numero != 0) {
+        invertedNumber = invertedNumber * 10 + numero % 10;
+        numero /= 10;
+    }
+
+    return invertedNumber;
+}
