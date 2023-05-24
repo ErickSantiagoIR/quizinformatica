9b3a39728a07d3a7182ff4ca68020e56610d6c4f
ErickSantiagoIR <132624203+ErickSantiagoIR@users.noreply.github.com>
2023-05-23 19:25:50 -0500

    P3 - getInvertNumbe
    Punto 3 Quiz

--- quiz1.c
+++ quiz1.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int main(void){
-    char getRevertText, palabra[21];
-    int i;
-    printf("Ingrese un palabra");
-    scanf("%s",palabra);
-    i=0;
-    while(palabra[i++]!= '\0');
-    printf("%s al reves es: ",palabra);
+#include<stdlib.h>
+
+int getInvertNumber(int numero);
+
+int main() {
+    int numero;
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
     
-    while(i>=0)
-        printf("%s",palabra[i--]);
-        printf("\n");
-        system("pause");
-return 0;
+    int invertedNumber = getInvertNumber(numero);
+
+    printf("Número invertido: %d\n", invertedNumber);
+
+    return 0;
+}
+
+int getInvertNumber(int numero) {
+    int invertedNumber = 0;
+
+    while (numero != 0) {
+        invertedNumber = invertedNumber * 10 + numero % 10;
+        numero /= 10;
+    }
+
+    return invertedNumber;
 }
